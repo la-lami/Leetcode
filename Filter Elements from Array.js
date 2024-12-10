52bd780949941d82db8e6af54a734dcda20af65d
Lamiya Fathima <lamiyarafeek786@gmail.com>
2024-12-10 16:10:49 +0530

    leetcode

---
+++ Filter Elements from Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+ 
+ 
+var filter = function(arr, fn) {
+    let arr1=[];
+    for(let i=0;i<=arr.length-1;i++){
+     if(fn(arr[i],i)){
+        arr1.push(arr[i]);
+     }
+    }
+    return arr1;
+    
+};
---
+++ Transform.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+var map = function(arr, fn) {
+    let arr1=[];
+    for(let i=0;i<arr.length;i++){
+        arr1.push(fn(arr[i],i));    
+    }
+    return arr1;
+    
+};
